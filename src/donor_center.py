b449058fdc566a5bea81b6a1e62e1fa0d8b45192
unknown <cocochino@gmail.com>
2023-08-04 14:57:39 -0400

    Adding donor center class and test

---
+++ src/donor_center.py
@@ -0,0 +1,15 @@
+'''
+Created on Aug 4, 2023
+
+@author: Miho
+Blood donor center functions
+'''
+
+registory = ()
+
+def show_options():
+    pass
+
+def show_registry():
+    for donor, bloodtype in registory:
+        print(f'  {donor}')
---
+++ test/system/test_donor_cener.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+class DonorCenterTest(TestCase):
+    
+    def test_show_registry(self):
+        ''' use mock print process to workaround this'''
