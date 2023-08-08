23a9061a8fc998862678c3d1b6174cbba9585f91
unknown <cocochino@gmail.com>
2023-08-08 17:42:37 -0400

    Additional donor center tests

--- src/donor_center.py
+++ src/donor_center.py
@@ -5,6 +5,9 @@ Created on Aug 4, 2023
 Blood donor center functions
 '''
 
+from donor_list import DonorList
+import donor
+
 OPTIONS = "Welcome to Blood Donation Center \
 Enter c to create a donor list, \
 p to existing donor lists, \
@@ -13,42 +16,60 @@ a to add a donor to a list, \
 q to quit. "
 
 NO_DONOR = 'No donors.'
-
-registory = ()
+lists = dict() #blogs
 
 def show_options():
-    print_regster_lists()
+    print_donor_lists()
     selection = input(OPTIONS)
     while selection != 'q':
-        if selection == 'c':
-            create_donot_register()
-        elif selection == 'l':
-            print_regster_lists()
-        elif selection == 'r':
-            show_donor_register()
-        elif selection == 'p':
+        if selection == 'c': #ask_create_blog()
+            create_donor_list()
+        elif selection == 'l': #print_blogs()
+            print_donor_lists()
+        elif selection == 'r': #ask_read_blog()
+            read_donor()
+        elif selection == 'p':#ask_create_post()
             add_donor()
         selection = input(OPTIONS)
+
         
-        
-def create_donot_register():
-    pass
-
-
-def show_donor_register():
-    pass
-
-
-def add_donor():
-    registory
-    pass
-
-
-def print_regster_lists():
+def print_donor_lists(): #print_blogs()
     ''' If registory is empty, return a generic message'''
-    if registory:
-        for donor, bloodtype in registory:
-            print(f'  {donor}-{bloodtype}')
+    if lists:
+        for k, l in lists.items():
+            print(f'{k} - {l}')
     else:
         print(NO_DONOR)
+
+            
+def create_donor_list():  #ask_create_blog()
+    loc = input('Enter donor center location: ')
+    sv = input('Enter supervisor name: ')
+    lists[loc] = DonorList(loc, sv)
+    #print(lists[loc])
     
+
+def read_donor(): #ask_read_blog()
+    title = input('Enter the donor list to be displayed: ')
+    print_donors(lists[title])
+
+
+def print_donors(dLlist): #print_posts
+    for l in dLlist.donors:
+        #print_donor("inside1", list)
+        #print("list contest is: ", lists[list])
+        print_donor(l)
+
+
+def print_donor(donor): #print_post
+    print(f"Donor info: {donor.name} {donor.blood_type}")
+
+
+def add_donor():#ask_create_post()
+    theList = input('Enter donor list name: ')
+    donor_name = input('Enter donor name: ')
+    blood_type = input('Enter donor blood type: ')
+    lists[theList].create_blood_donor(donor_name, 'active', blood_type )
+    #pass
+
+
--- test/system/test_donor_cener.py
+++ test/system/test_donor_cener.py
@@ -9,31 +9,97 @@ Mocking saves test execution time too
 from unittest import TestCase
 from unittest.mock import patch
 
+from donor_list import DonorList
+from blood_donor import BloodDonor
+
 import donor_center as dc
+
 class DonorCenterTest(TestCase):
     
-    #Mock testing
-    def test_print_regster_lists_empty(self):
-        ''' Example 1. Use mocked print to verify specific text was called'''
-        with patch('builtins.print') as mock_print:
-            dc.print_regster_lists()
-            mock_print.assert_called_with(dc.NO_DONOR)
-
-    ''' Example 2. Use mocked print to verify class constant text was called '''
-    '''  
-    def test_show_options(self):
+    
+    #Mock testing   
+    def test_1_show_options(self):
+        ''' Example 2. Use mocked print to verify class constant text was called '''
         with patch('builtins.print') as mock_print:
             with patch('builtins.input', return_value = 'q') as mock_input:
                 dc.show_options()
                 mock_print.assert_called_with(dc.NO_DONOR)
-    '''
-                   
+
+                       
+    def test_2_print_donor_lists_empty(self):
+        ''' Example 1. Use mocked print to verify specific text was called'''
+        with patch('builtins.print') as mock_print:
+            dc.print_donor_lists()
+            mock_print.assert_called_with(dc.NO_DONOR)
+
+
+    def test_option_create_donor_list(self):
+        with patch('builtins.input') as mock_input:
+            mock_input.side_effect = ('c', 'Boston', 'SV1', 'q')
+            dc.show_options()
+            self.assertIsNotNone(dc.lists['Boston'])
+    
+    
     def test_show_options_display_registry(self):
         ''' Example 3-1 Use mocked  to check if custom method was called. NOTE: can't use import as short-hand
             Example 3-2 Mocking user input '''
-        with patch('donor_center.print_regster_lists') as mock_print_regster_lists:
+        with patch('donor_center.print_donor_lists') as mock_print_regster_lists:
             #In below example, test feeds 'x' to any input requests 
-            with patch('builtins.input', return_value = 'q') as mock_input:
-                dc.print_regster_lists()
+            with patch('builtins.input', return_value='q') as mock_input: #This will always return 'q' as input
+                dc.print_donor_lists()
                 mock_print_regster_lists.assert_called()
+
+
+    def test_create_donor_list(self):#ask_create_blog
+        '''Example 4 -  Handling multiple input by using side_effect'''
+        with patch('builtins.input') as mocked_input:
+            #mocked_input.side_effect = ('Test', 'Author')
+            mocked_input.side_effect = ('Newton', 'Preston S') #Multiple input will be sent to test in this order
+            dc.create_donor_list()
+            #self.assertIsNotNone(dc.create_donor_list('Newton'))
+            self.assertIsNotNone('Newton')
+            self.assertIsNotNone('Preston S')
+                 
+     
+    def test_read_donor(self): #ask_read_blog()
+        '''Example 5 Handling list'''
+        dList = DonorList('Newton', 'Preston S')
+        dc.lists = {'Test register':dList} # This test list will be called below
+        with patch('builtins.input', return_value='Test register') as mock_input:
+            with patch('donor_center.print_donors') as mock_print_donors:
+                dc.read_donor()
+                mock_print_donors.assesrt_called_with(dList)
+                
+                
+    def test_print_donors(self):
+        ''' Example 6 - calling embedded method '''
+        dList = DonorList('Newton', 'Preston S')
+        dList.create_blood_donor('Donor 1', 'active','B')
+        #print("dList 0 is", dList.donors[0])
+        with patch('donor_center.print_donor') as mock_print_donor:
+            dc.print_donors(dList)
+            mock_print_donor.assert_called_with(dList.donors[0])
+            
+            
+    def test_print_donor(self):
+        name = 'Donor 1'
+        blood_type = 'B'
+        donor = BloodDonor(name, 'active', blood_type)
+        expected = f"Donor info: {name} {blood_type}"
+        with patch('builtins.print') as mocked_print:
+            dc.print_donor(donor)
+            mocked_print.assert_called_with(expected)
+            
+            
+    def test_add_donor(self):
+        dList = DonorList('Newton', 'Preston S')
+        dc.lists = {'Test register':dList} # This test list will be called below
+        with patch('builtins.input') as mocked_input:
+            mocked_input.side_effect = ('Test register', 'Donor 2', 'AB')
+            dc.add_donor()
+            self.assertEqual(dList.donors[0].name, 'Donor 2')
+            self.assertEqual(dList.donors[0].status, 'active')
+            self.assertEqual(dList.donors[0].blood_type, 'AB')
+            self.assertEqual(list(dc.lists.keys())[0], 'Test register')
+
 
